e167bcec88e69e022d494a6e99eccb5edc2245b5
David A. Cuadrado <krawek@gmail.com>
2014-10-18 15:05:59 -0500

    Fix payload controller.

--- api/controllers/payload.rb
+++ api/controllers/payload.rb
@@ -1,16 +1,16 @@
-Gamegit::App.controller :payload do
+Gamegit::Api.controller :payload do
   before do
     content_type :json
   end
 
   helpers do
     def verify_signature(payload_body)
-      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['GAMEGIT_SECRET_TOKEN'], payload_body)
-      return halt 500, {error: "Signatures didn't match!"}.to_json unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['GAMEGIT_SECRET_TOKEN'].to_s, payload_body)
+      return halt 500, {error: "Signatures didn't match!"}.to_json unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'].to_s)
     end
   end
 
-  post "/github" do
+  post :create, map: "/github" do
     request.body.rewind
     payload_body = request.body.read
     verify_signature(payload_body)
