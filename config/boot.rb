e393c0c48e56cc69cc5a48451493e0d642ff63f0
David A. Cuadrado <krawek@gmail.com>
2014-10-18 15:11:02 -0500

    Log to stdout on production.

--- config/boot.rb
+++ config/boot.rb
@@ -10,9 +10,9 @@ Bundler.require(:default, RACK_ENV)
 ##
 # ## Enable devel logging
 #
-# Padrino::Logger::Config[:development][:log_level]  = :devel
-# Padrino::Logger::Config[:development][:log_static] = true
-#
+Padrino::Logger::Config[:production][:log_level] = :info
+Padrino::Logger::Config[:production][:stream] = :stdout
+
 # ## Configure your I18n
 #
 # I18n.default_locale = :en
