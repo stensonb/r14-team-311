f5f41cc9e25d6c6f2fbc6c363752eb7f00df7e71
David A. Cuadrado <krawek@gmail.com>
2014-10-18 15:59:27 -0500

    Add helper for the api response.

--- api/controllers/payload.rb
+++ api/controllers/payload.rb
@@ -4,16 +4,15 @@ Gamegit::Api.controller :payload do
   end
 
   post :create, map: "/github" do
-    request.body.rewind
-    payload_body = request.body.read
-    verify_signature(payload_body)
+    payload_body = read_body_from_request
+    verify_github_signature(payload_body)
 
     event = Event.build_from_payload(request.env, payload_body)
 
     if event.save
-      {success: true }.to_json
-    else
-      halt 403, {success: false}.to_json
+      api_response 200
     end
+
+    api_response 403
   end
 end
