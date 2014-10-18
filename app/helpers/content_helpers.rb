faca0bbc0110db4cee954342a4f9aa5e174ae7c1
David A. Cuadrado <krawek@gmail.com>
2014-10-18 17:44:34 -0500

    Allow XSS on development.

--- app/helpers/content_helpers.rb
+++ app/helpers/content_helpers.rb
@@ -1,12 +1,27 @@
 Gamegit::App.helpers do
   def angular_response(code, content)
-    halt code, {"Content-Type" => "application/json"}, normalize_content_for_angular(content)
+    halt code, headers_for_angular, normalize_content_for_angular(content)
+  end
+
+  def headers_for_angular
+    headers = {
+      "Content-Type" => "application/json"
+    }
+
+    if Padrino.env == :development
+      # Allow XSS in development.
+      headers["Access-Control-Allow-Origin"] = "*"
+    end
+
+    headers
   end
 
   def normalize_content_for_angular(content)
     response = {}
+
     response["data"] = content.as_json
     response["metadata"] = get_metadata_for_response(response["data"])
+
     response.to_json
   end
 
