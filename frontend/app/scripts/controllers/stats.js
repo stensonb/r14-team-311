ad29a82c86685ec19c3bec56fb47daab1fbb1802
Jorge Cuadrado <kuadrosxx@gmail.com>
2014-10-19 12:15:19 -0500

    Improve stats panel style and add fake stats data
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- frontend/app/scripts/controllers/stats.js
+++ frontend/app/scripts/controllers/stats.js
@@ -9,21 +9,87 @@
  */
 angular.module('frontendApp')
   .controller('StatsCtrl', function ($scope, Restangular) {
-    $scope.stats = {};
+    var before;
+    $scope.cStat = 'points';
+    $scope.stats = [];
+
+    function createFakeStats() {
+      return {
+        assigned_issues: Math.round(Math.random()*20),
+        closed_issues: Math.round(Math.random()*20),
+        closed_pull_request: Math.round(Math.random()*20),
+        closed_pull_requests: Math.round(Math.random()*20),
+        commits: Math.round(Math.random()*20),
+        opened_issues: Math.round(Math.random()*20),
+        opened_pull_request: Math.round(Math.random()*20),
+        opened_pull_requests: Math.round(Math.random()*20),
+        points: Math.round(Math.random()*20),
+        pushes: Math.round(Math.random()*20)
+      };
+    }
 
     Restangular.one('stats', 'monthly').getList().then(function(stats){
-      for (var i = 0; i < stats.length; i++) {
-        var stat = stats[i];
-        for (var k in stat) {
-          $scope.stats[k] = $scope.stats[k] || [];
-          $scope.stats[k].push(stat[k]);
-        }
-      }
-      for (var key in $scope.stats) {
-        var s = $scope.stats[key];
-        if (s.length <= 1) {
-          $scope.stats[key].unshift(0);
-        }
+      $scope.stats = stats;
+      $scope.current = $scope.stats[$scope.stats.length-1];
+      before = $scope.stats[$scope.stats.length-2];
+      if ($scope.stats.length <= 1) {
+        before = createFakeStats();
+        $scope.stats.unshift(before);
+        $scope.stats.unshift(createFakeStats());
+        $scope.stats.unshift(createFakeStats());
+        $scope.stats.unshift(createFakeStats());
       }
     });
+
+    $scope.extractData = function(key) {
+      var rs = [];
+      for (var i = 0; i < $scope.stats.length; i++) {
+        var stat = $scope.stats[i];
+        rs.push(stat[key]);
+      }
+      return rs;
+    };
+
+    $scope.getDelta = function(key) {
+      if (!$scope.current) {
+        return 0;
+      }
+
+      var beforeData = 0;
+      if (before) {
+        beforeData = before[key];
+      }
+      var rs = $scope.current[key] - beforeData;
+      if (rs > 0) {
+        rs = '+ ' + rs;
+      } else {
+        rs = '- ' + Math.abs(rs);
+      }
+      return rs;
+    };
+
+    var labels = {
+      assigned_issues: 'assigned_issues',
+      closed_issues: 'closed issues',
+      closed_pull_request: 'closed pull request',
+      closed_pull_requests: 'closed pull requests',
+      commits: 'commits',
+      opened_issues: 'opened issues',
+      opened_pull_request: 'opened pull request',
+      opened_pull_requests: 'opened pull requests',
+      points: 'points',
+      pushes: 'pushes'
+    };
+
+    $scope.getLabel = function(key) {
+      return labels[key];
+    };
+
+    $scope.changeCstat = function(key) {
+      $scope.cStat = key;
+    };
+
+    $scope.isActive = function(key) {
+      return $scope.cStat === key;
+    };
   });
