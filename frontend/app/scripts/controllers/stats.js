239c4a956ee76c5abd0d3bfc357ad36ec31d8a5e
Jorge Cuadrado <kuadrosxx@gmail.com>
2014-10-18 23:20:39 -0500

    Implements stats view
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- frontend/app/scripts/controllers/stats.js
+++ frontend/app/scripts/controllers/stats.js
@@ -8,10 +8,22 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('StatsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('StatsCtrl', function ($scope, Restangular) {
+    $scope.stats = {};
+
+    Restangular.one('stats', 'monthly').getList().then(function(stats){
+      for (var i = 0; i < stats.length; i++) {
+        var stat = stats[i];
+        for (var k in stat) {
+          $scope.stats[k] = $scope.stats[k] || [];
+          $scope.stats[k].push(stat[k]);
+        }
+      }
+      for (var key in $scope.stats) {
+        var s = $scope.stats[key];
+        if (s.length <= 1) {
+          $scope.stats[key].unshift(0);
+        }
+      }
+    });
   });
