c9040beb329c815973df256392a7688d6a72da5d
Jorge Cuadrado <kuadrosxx@gmail.com>
2014-10-18 19:46:22 -0500

    Adds momentjs to handle dates and use it on live stream
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- frontend/app/scripts/controllers/live_stream.js
+++ frontend/app/scripts/controllers/live_stream.js
@@ -9,10 +9,22 @@
  */
 angular.module('frontendApp')
   .controller('LiveStreamCtrl', function ($scope, $timeout, Restangular) {
+    var timestamp = 0;
     (function tick() {
-      Restangular.all('events').getList().then(function(events) {
+      Restangular.all('events').getList({timestamp: timestamp}).then(function(events) {
+        for(var i = 0; i < events.length; i++) {
+          var evnt = events[i];
+          evnt.created_at = moment(evnt.created_at);
+          if (evnt.type === 'push') {
+            for(var j = 0; j < evnt.data.commits.length; j++) {
+              var commit = evnt.data.commits[j];
+              commit.timestamp = moment(commit.timestamp);
+            }
+          }
+        }
+        timestamp = events[0].created_at.unix();
         $scope.events = events;
-        $timeout(tick, 1000);
+        $timeout(tick, 5000);
       });
     })();
   });
