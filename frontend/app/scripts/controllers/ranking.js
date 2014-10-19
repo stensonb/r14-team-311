c01bfc71c3798c9aaf096da29163dedd0439e208
Jorge Cuadrado <kuadrosxx@gmail.com>
2014-10-18 22:25:40 -0500

    Shows ranking leader stats
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- frontend/app/scripts/controllers/ranking.js
+++ frontend/app/scripts/controllers/ranking.js
@@ -11,6 +11,11 @@ angular.module('frontendApp')
   .controller('RankingCtrl', function ($scope, Restangular) {
     Restangular.all('users').getList().then(function(users) {
       $scope.leader = users[0];
+
+      Restangular.one('user_stats', $scope.leader.login).getList('monthly').then(function(stats){
+        $scope.leaderStats = stats[stats.length-1];
+      });
+
       if (users.length > 1) {
         $scope.rest = users.slice(1, users.length);
       }
