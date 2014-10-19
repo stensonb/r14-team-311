0ed9d74081a4c630d62d965fb41801f533371e5b
Jorge Cuadrado <kuadrosxx@gmail.com>
2014-10-18 21:13:48 -0500

    Adds about dialog
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $modal) {
+    $scope.openAbout = function() {
+      $modal.open({
+        templateUrl: 'views/about.html',
+        controller: function($scope, $modalInstance) {
+          $scope.close = function() {
+            $modalInstance.dismiss('cancel');
+          };
+        }
+      });
+    };
   });
