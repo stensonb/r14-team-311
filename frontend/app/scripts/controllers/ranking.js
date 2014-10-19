dd1b12800e4a2b46195a1f19aec2410d30c80fc5
Jorge Cuadrado <kuadrosxx@gmail.com>
2014-10-19 18:35:59 -0500

    Improve ranking
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- frontend/app/scripts/controllers/ranking.js
+++ frontend/app/scripts/controllers/ranking.js
@@ -9,25 +9,46 @@
  */
 angular.module('frontendApp')
   .controller('RankingCtrl', function ($scope, $rootScope, Restangular) {
-    function getLeaderStats() {
-      if ($scope.leader) {
-        Restangular.one('user_stats', $scope.leader.login).getList($rootScope.g).then(function(stats){
-          $scope.leaderStats = stats[stats.length-1];
+    $scope.stats = {};
+    function getUserStats(users) {
+      for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        Restangular.one('user_stats', user.login).getList($rootScope.g).then(function(stats){
+          $scope.stats[stats[0].login] = stats[0];
         });
       }
     }
 
+    var icons = {};
+    var labels = {};
+    Restangular.one('achievements', 'types').getList().then(function(types) {
+      for (var i = 0; i < types.length; i++) {
+        var achievement = types[i];
+        icons[achievement.id] = achievement.image_url;
+        labels[achievement.id] = achievement.name;
+      }
+    });
+
+    $scope.getAchievementIcon = function(id) {
+      return icons[id];
+    };
+
+    $scope.getAchievementLabel = function(id) {
+      return labels[id];
+    };
+
     $rootScope.$watch('g', function () {
-      getLeaderStats();
+      if ($scope.users) {
+        getUserStats($scope.users);
+      }
     });
 
     Restangular.all('users').getList().then(function(users) {
-      $scope.leader = users[0];
-
-      getLeaderStats();
-
-      if (users.length > 1) {
-        $scope.rest = users.slice(1, users.length);
-      }
+      $scope.users = users;
+      getUserStats($scope.users);
     });
+
+    $scope.fromNow = function (date) {
+      return moment(date).fromNow();
+    };
   });
