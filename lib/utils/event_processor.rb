95fe7b14825ec78e711de56e8d75020651124bd0
Sergio Aristizábal Gómez <serargz@gmail.com>
2014-10-18 19:30:05 -0500

    Fixed stats increment.

--- lib/utils/event_processor.rb
+++ lib/utils/event_processor.rb
@@ -20,45 +20,45 @@ class EventProcessor
   def process_push_event(event)
     # TODO: Process event specific content action
     points = event.user.points
-    event.user.set(points + Event::POINTS[:push])
-    Stats.current_stats.inc(:opened_issues)
+    event.user.inc(points: Event::POINTS[:push])
+    Stats.current_stats.inc(opened_issues: 1)
   end
 
   def process_ping_event(event)
     # TODO: Process event specific content action
     points = event.user.points
-    event.user.set(points + Event::POINTS[:ping])
+    event.user.inc(points: Event::POINTS[:ping])
   end
 
   def process_issues_event(event)
     # TODO: Process event specific content action
     points = event.user.points
-    event.user.set(points + Event::POINTS[:issues])
+    event.user.inc(points: Event::POINTS[:issues])
 
     action = event.data["action"]
     if action == "opened"
-      Stats.current_stats.inc(:opened_issues)
+      Stats.current_stats.inc(opened_issues: 1)
     elsif action == "closed"
-      Stats.current_stats.inc(:closed_issues)
+      Stats.current_stats.inc(closed_issues: 1)
     end
   end
 
   def process_create_event(event)
     # TODO: Process event specific content action
     points = event.user.points
-    event.user.set(points + Event::POINTS[:create])
+    event.user.inc(points: Event::POINTS[:create])
   end
 
   def process_pull_request_event(event)
     # TODO: Process event specific content action
     points = event.user.points
-    event.user.set(points + Event::POINTS[:pull_request])
+    event.user.inc(points: Event::POINTS[:pull_request])
 
     action = event.data["action"]
     if action == "opened"
-      Stats.current_stats.inc(:opened_pull_requests)
+      Stats.current_stats.inc(opened_pull_requests: 1)
     elsif action == "closed"
-      Stats.current_stats.inc(:closed_pull_requests)
+      Stats.current_stats.inc(closed_pull_requests: 1)
     end
   end
 
