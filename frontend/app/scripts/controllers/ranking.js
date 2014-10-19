494e498c311a7fec27aed15b0d117e84cb8697f6
Jorge Cuadrado <kuadrosxx@gmail.com>
2014-10-18 21:13:19 -0500

    Fix ranking
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- frontend/app/scripts/controllers/ranking.js
+++ frontend/app/scripts/controllers/ranking.js
@@ -11,6 +11,8 @@ angular.module('frontendApp')
   .controller('RankingCtrl', function ($scope, Restangular) {
     Restangular.all('users').getList().then(function(users) {
       $scope.leader = users[0];
-      $scope.rest = users.slice(0, users.length);
+      if (users.length > 1) {
+        $scope.rest = users.slice(1, users.length);
+      }
     });
   });
